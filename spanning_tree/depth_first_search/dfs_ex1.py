f6ec7b57c51a642eb2631be247a27999f3311b4e
tom_liakos <tom.liakos@oracle.com>
2018-12-13 18:06:58 -0800

    updating paths for spanning tree

---
+++ spanning_tree/depth_first_search/dfs_ex1.py
@@ -0,0 +1,11 @@
+# DFS Algorithm
+# Traversing or searching tree or graph
+# data structures. The algo starts at root
+# node and explores as far as possible
+# along each branch before backtracking.
+
+# Great for traversing a graph, produces
+# minimum spanning tree and all pair shortest
+# path tree.
+
+
